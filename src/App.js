6e2fde9e0717525487aa3a178d30915622849f3e
sehrish97 <57295611+sehrish97@users.noreply.github.com>
2021-07-06 21:54:27 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import React from 'react';
+import Grid from './components/Grid';
+function App() {
+  return (
+    <div >
+       <Navbar />
+       <Grid />
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/adminreducer.js
@@ -0,0 +1,7 @@
+const INITIAL_STATE ={
+    name: "sehrish"
+}
+const reducer =(state = INITIAL_STATE)=> {
+    return state
+}
+export default reducer;
